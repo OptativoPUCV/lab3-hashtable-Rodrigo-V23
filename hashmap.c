a66952358fd2dc2ac6b1688ad0a4008b876e80a0
Replit user <>
2023-09-08 18:03:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -15,14 +15,12 @@ struct HashMap {
     long capacity; //capacidad de la tabla
     long current; //indice del ultimo dato accedido
 };
-
 Pair * createPair( char * key,  void * value) {
     Pair * new = (Pair *)malloc(sizeof(Pair));
     new->key = key;
     new->value = value;
     return new;
 }
-
 long hash( char * key, long capacity) {
     unsigned long hash = 0;
      char * ptr;
@@ -31,14 +29,11 @@ long hash( char * key, long capacity) {
     }
     return hash%capacity;
 }
-
 int is_equal(void* key1, void* key2){
     if(key1==NULL || key2==NULL) return 0;
     if(strcmp((char*)key1,(char*)key2) == 0) return 1;
     return 0;
 }
-
-
 void insertMap(HashMap * map, char * key, void * value) {
 
 
@@ -51,9 +46,10 @@ void enlarge(HashMap * map) {
 }
 
 
-HashMap * createMap(long capacity) {
-
-    return NULL;
+HashMap * createMap(long capacity)
+{
+  
+  return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
